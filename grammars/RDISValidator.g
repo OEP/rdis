d6576bb91228a9c829354127d8c63bff260cd826
Paul Kilgo <paulkilgo@gmail.com>
2011-10-17 00:31:27 -0500

    Write a remote debugger for the treewalker; validator now validates the default AST type returned from the parser

--- grammars/RDISValidator.g
+++ grammars/RDISValidator.g
@@ -6,3 +6,71 @@ options {
 	output=AST;
 }
 
+@header {
+package edu.ua.cs.rdis.gen;
+}
+
+rdis
+	: ^(ROBOT object)
+	;
+	
+object
+	: ^(OBJECT definition*)
+	;
+
+list
+	: ^(LIST value*)
+	;
+
+definition
+	: ^(keyword value)
+	;
+
+keyword
+	: NAME
+	| INTERVAL
+	| SPEED
+	| TYPE
+	| VALUE
+	| ON_START
+	| ON_TERMINATE
+	| ON_KEEPALIVE
+	| STATE
+	| CONNECTIONS
+	| SPP
+	| INT
+	| STRING
+	| FLOAT
+	| PRIMITIVES
+	| SIGNATURE
+	| WRITE_FORMAT
+	| FORMAT
+	| READ_FORMAT
+	| REGEX
+	| PRIMITIVE
+	| PARAMETERS
+	| INTERFACES
+	| EXPRESSION
+	| RETURNS
+	;
+	
+value
+	: string
+	| number
+	| identifier
+	| list
+	| object
+	| keyword
+	;
+
+identifier
+	: ^(IDENTIFIER Identifier)
+	;
+
+string
+	: ^(STRING String)
+	;
+	
+number
+	: ^(NUMBER Number Exponent?)
+	;
---
+++ src/edu/ua/cs/rdis/Validator.java
@@ -0,0 +1,47 @@
+package edu.ua.cs.rdis;
+
+import org.antlr.runtime.ANTLRFileStream;
+import org.antlr.runtime.ANTLRInputStream;
+import org.antlr.runtime.CharStream;
+import org.antlr.runtime.CommonTokenStream;
+import org.antlr.runtime.tree.CommonTree;
+import org.antlr.runtime.tree.CommonTreeNodeStream;
+
+import edu.ua.cs.rdis.gen.RDISLexer;
+import edu.ua.cs.rdis.gen.RDISParser;
+import edu.ua.cs.rdis.gen.RDISValidator;
+
+public class Validator
+{
+    public static void main(String[] args) throws Exception {
+    	if(args.length == 0) {
+    		System.out.println("Usage: java Validator <file>");
+    		System.exit(1);
+    	}
+
+      CommonTokenStream tokens = new CommonTokenStream();
+      {
+    	 String file = args[0];
+    	 CharStream input = new ANTLRFileStream(file);
+         RDISLexer lexer = new RDISLexer(input);
+         tokens.setTokenSource(lexer);
+      }
+
+      CommonTreeNodeStream nodes;
+      {
+         RDISParser parser = new RDISParser(tokens);
+         RDISParser.rdis_return example = parser.rdis();
+
+         CommonTree tree = (CommonTree)example.getTree();
+         nodes = new CommonTreeNodeStream(tree);
+      }
+
+      {
+         RDISValidator walker = new RDISValidator(nodes);
+         RDISValidator.rdis_return example = walker.rdis();
+         System.out.println(example.toString());
+      }
+      System.exit(0);
+
+    }
+}
